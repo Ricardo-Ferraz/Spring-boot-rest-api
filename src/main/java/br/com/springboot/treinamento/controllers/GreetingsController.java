f184a5b3226dec8c3540f484e4146b654c07e26b
Ricardo-Ferraz <ricardoferrazfilho4@gmail.com>
2021-12-13 16:03:55 -0300

    Initial commit

---
+++ src/main/java/br/com/springboot/treinamento/controllers/GreetingsController.java
@@ -0,0 +1,40 @@
+package br.com.springboot.treinamento.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.treinamento.model.Usuario;
+import br.com.springboot.treinamento.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    
+	@Autowired //Injeção de dependencia
+	private UsuarioRepository usuarioRepository;
+	
+    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    
+    @RequestMapping(value= "/olamundo/{nome}", method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	this.usuarioRepository.save(usuario); //Grava no banco
+    	
+    	return "Olá mundo "+nome;
+    }
+}
